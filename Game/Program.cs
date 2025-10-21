24c5a946ce4e650316efcaf82f03aa942815536f
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-10-21 16:39:52 +0300

    Add project files.

---
+++ Game/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cursorLeft = 0;
+            int cursorTop = 0;
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.A:
+                        Console.Clear();
+                        cursorLeft--;
+                        Console.SetCursorPosition(cursorLeft, cursorTop);
+                        Console.Write("@");
+                        break;
+
+                    case ConsoleKey.S:
+                        Console.Clear();
+                        cursorLeft++;
+                        Console.SetCursorPosition(cursorLeft, cursorTop);
+                        Console.Write("@");
+                        break;
+
+                    case ConsoleKey.W:
+                        Console.Clear();
+                        cursorTop--;
+                        Console.SetCursorPosition(cursorLeft, cursorTop);
+                        Console.Write("@");
+                        break;
+
+                    case ConsoleKey.Z:
+                        Console.Clear();
+                        cursorTop++;
+                        Console.SetCursorPosition(cursorLeft, cursorTop);
+                        Console.Write("@");
+                        break;
+
+                    default:
+                        string input = "";
+                        input += keyInfo.KeyChar;
+                        Console.Write(input);
+                        break;
+                }
+            }
+        }
+    }
+}
