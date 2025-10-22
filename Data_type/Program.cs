0d8fac8ff592b727c0b4804783f81156d4c4e4a8
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-10-22 19:00:05 +0300

    Pacman done

---
+++ Data_type/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тип данных в количестве байтах :");
+            Console.WriteLine($@"{typeof(byte)}: {sizeof(byte)} байт");
+            Console.WriteLine($@"{typeof(sbyte)}: {sizeof(sbyte)} байт");
+            Console.WriteLine($@"{typeof(ushort)}: {sizeof(ushort)} байт");
+            Console.WriteLine($@"{typeof(int)}: {sizeof(int)} байт");
+            Console.WriteLine($@"{typeof(uint)}: {sizeof(uint)} байт");
+            Console.WriteLine($@"{typeof(ulong)}: {sizeof(ulong)} байт");
+            Console.WriteLine($@"{typeof(float)}: {sizeof(float)} байт");
+            Console.WriteLine($@"{typeof(double)}: {sizeof(double)} байт");
+            Console.WriteLine($@"{typeof(decimal)}: {sizeof(decimal)} байт");
+
+            //Console.WriteLine($"Int "); sizeof(int);
+        }
+
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static int fieldSize = 10;
+        static char[,] PlayerField = new char[fieldSize, fieldSize];
+        
+        static void Main(string[] args)
+        {
+
+            InitializateField(PlayerField);
+         
+
+            PrintField(PlayerField, "Уровень 1:");
+
+        }
+
+        
+        static void InitializateField(char[,] field)
+        {
+            for (int i = 0; i < fieldSize; ++i)
+            {
+                for (int j = 0; j < fieldSize; ++j)
+                {
+                    field[i, j] = '*';
+                }
+            }
+        }
+        static void PrintField(char[,] field, string level_name)
+        {
+            Console.WriteLine(level_name);
+            Console.Write("  ");
+            for (int i = 0; i < fieldSize; ++i)
+            {
+                Console.Write($"{i + 1} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < fieldSize; ++i)
+            {
+                Console.Write($"{(char)('A' + i)} ");
+                for (int j = 0; j < fieldSize; ++j)
+                {
+                    Console.Write($"{field[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+    }
+}
